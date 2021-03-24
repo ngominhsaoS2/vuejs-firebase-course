5bf37befc31a8a8d7346ab3aec890dbb3dae155b
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-03-24 20:57:53 +0700

    43. CHALLENGE - Deleting Skills

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -15,7 +15,7 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{skill}}
+      <span @click="deleteSkill(skill)">{{skill}}</span>
     </div>
 
     <div class="terms">
@@ -51,6 +51,11 @@ export default {
 
           this.tempSkill = '';
         }
+      },
+      deleteSkill(skill) {
+        this.skills = this.skills.filter((item) => {
+          return item !== skill;
+        });
       }
     }
 }
@@ -89,5 +94,17 @@ export default {
     margin: 0 10px 0 0;
     position: relative;
     top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
   }
 </style>
