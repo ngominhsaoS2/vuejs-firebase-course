84da0770e80be1f9702987332703918c1cb0f613
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-03-26 19:00:43 +0700

    47. Router Links

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure accusantium tenetur culpa at nihil soluta aliquam qui pariatur sunt cupiditate. Autem ullam accusamus distinctio atque odit aliquid ut quibusdam id, labore dolores dolorem eaque cumque nulla sint tempore incidunt non eveniet fugiat modi facilis aperiam dolorum tempora. Molestiae ex, doloremque non possimus impedit beatae praesentium ab, illo velit vel asperiores alias rem sint. Error fugiat distinctio labore eveniet dicta mollitia omnis possimus neque aspernatur, praesentium voluptate dolorum tenetur, aliquid, quibusdam ipsam id atque. Consectetur aut dicta esse ut et mollitia error molestiae autem, dolores voluptatum, asperiores atque, recusandae quibusdam quaerat?</p>
   </div>
 </template>
