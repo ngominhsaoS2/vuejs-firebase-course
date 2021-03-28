a5b1dd1bf60cc2b83e73513af7bc1d0ceed97d62
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-03-28 16:48:54 +0700

    49. Route Parameters

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <h1>Job Details Page</h1>
+  <p>The Job Details id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
 }
 </script>
 
