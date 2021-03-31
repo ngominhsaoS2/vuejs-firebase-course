7b4dc822b2841ba6a05b55bb6d9bd90039cae3a2
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-03-31 19:40:12 +0700

    71. Composition API Basics

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  setup() {
+    let name = 'saonm';
+    let age = 26;
+
+    const handleClick = () => {
+      console.log("You've just clicked me!")
+    }
+
+    return { name, age, handleClick }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    border: none;
+    background-color: rgb(75, 161, 75);
+    color: white;
+    padding: 5px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: 100;
+  }
+</style>
