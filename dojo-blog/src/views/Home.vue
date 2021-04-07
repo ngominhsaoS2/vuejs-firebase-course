d3222c729b61c02df5b9c2bd49ef89b95680667b
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-07 17:28:12 +0700

    80. Reusable Composition Functions (Composables)

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -9,30 +9,17 @@
 </template>
 
 <script>
-import { ref } from 'vue'
 // component imports
 import PostList from '../components/PostList.vue'
+// composables
+import getPosts from '../composables/getPosts'
+
 export default {
   name: 'Home',
   components: { PostList },
   setup() { 
-    const posts = ref([])
-    const error = ref(null)
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok) {
-          throw Error('no available data')
-        }
-        posts.value = await data.json()
-        console.log(posts.value)
-      }
-      catch(err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
-    load()
+    const { posts, error, load } = getPosts();
+    load();
     
     return { posts, error }
   },
