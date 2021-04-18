b0137a8c8a6c3567893f9a2ae450a9bc23bb8e77
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-18 17:03:28 +0700

    81. Creating Another Composable

--- dojo-blog/src/composables/getPosts.js
+++ dojo-blog/src/composables/getPosts.js
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 const getPosts = () => {
     const posts = ref([])
     const error = ref(null)
+    
     const load = async () => {
       try {
         let data = await fetch('http://localhost:3000/posts')
