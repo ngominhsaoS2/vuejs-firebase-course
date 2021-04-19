b006cf69dd57fe115771281c0c7fe2785419be45
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-19 20:50:19 +0700

    94. Fetching a Collection of Documents

--- dojo-blog/src/composables/getPosts.js
+++ dojo-blog/src/composables/getPosts.js
@@ -1,16 +1,19 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
     const posts = ref([])
     const error = ref(null)
+
     const load = async () => {
       try {
-        let data = await fetch('http://localhost:3000/posts')
-        if(!data.ok) {
-          throw Error('no available data')
-        }
-        posts.value = await data.json()
-        console.log(posts.value)
+        const res = await projectFirestore.collection('posts').get()
+        //console.log(res.docs)
+
+        posts.value = res.docs.map(doc => {
+          //console.log(doc.data())
+          return { ...doc.data(), id: doc.id }
+        })
       }
       catch(err) {
         error.value = err.message
