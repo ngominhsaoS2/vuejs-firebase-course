b446f5411d6e975ba7ac87e120b946682aaa3b2b
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-19 19:44:46 +0700

    92. Connecting to a Firebase Project

---
+++ dojo-blog/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqrwWBYVtMyVoVjzYPc9ex_KLrOuhLWGg",
+    authDomain: "fir-db-test-8821a.firebaseapp.com",
+    projectId: "fir-db-test-8821a",
+    storageBucket: "fir-db-test-8821a.appspot.com",
+    messagingSenderId: "683936388507",
+    appId: "1:683936388507:web:b3c61033df672d7124bd3e"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init firestore service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
