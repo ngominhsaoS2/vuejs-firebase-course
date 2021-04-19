c21acccc0df5db914f6aa9f525043c4025799baf
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-19 15:41:08 +0700

    85. Future Async Lesson & Extra Styling

--- dojo-blog/src/composables/getPosts.js
+++ dojo-blog/src/composables/getPosts.js
@@ -5,11 +5,6 @@ const getPosts = () => {
     const error = ref(null)
     const load = async () => {
       try {
-        // simulate delay
-        await new Promise(resolve => {
-          setTimeout(resolve, 2000)
-        })
-
         let data = await fetch('http://localhost:3000/posts')
         if(!data.ok) {
           throw Error('no available data')
