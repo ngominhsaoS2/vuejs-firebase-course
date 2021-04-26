f91d2c04103b11c77e47a3d0e571cc99a59cc690
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-26 09:36:09 +0700

    142. CHALLENGE - Logging Users Out

--- muso-ninjas/src/components/Navbar.vue
+++ muso-ninjas/src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <img src="@/assets/ninja.png"/>
       <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
       <div class="links">
-        <button>Logout</button>
+        <button @click="handleClick">Logout</button>
         <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
         <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
       </div>
@@ -13,7 +13,23 @@
 </template>
 
 <script>
+// challenge
+//   - fire a function called handleSubmit when the logout button is clicked
+//   - inside the function log the user out & then redirect to the login view
+import useLogout from '../composables/useLogout'
+import { useRouter } from 'vue-router'
+
 export default {
+  setup() {
+    const { logout } = useLogout()
+    const router = useRouter()
+    const handleClick = async () => {
+      await logout()
+      console.log('logged out')
+      router.push({ name: 'Login' })
+    }
+    return { handleClick }
+  }
 }
 </script>
 
@@ -42,7 +58,7 @@ export default {
   nav .links {
     margin-left: auto;
   }
-  
+
   nav .links a, button {
     margin-left: 16px;
     font-size: 14px;
