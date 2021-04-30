cbdef89e5bb5c58b01b2bee099b47ad6b0b1a31c
ngominhsaoS2 <ngominhsaoS2@gmail.com>
2021-04-30 10:22:56 +0700

    154. Redirecting to Playlist Details

--- muso-ninjas/src/views/playlists/CreatePlaylist.vue
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -20,23 +20,27 @@ import { ref } from 'vue'
 import useStorage from '@/composables/useStorage'
 import useCollection from '@/composables/useCollection'
 import getUser from '@/composables/getUser'
-import { timestamp } from '@/firebase/config'
+import { timestamp } from '@/firebase/config' 
+import { useRouter } from 'vue-router'
+
 export default {
   setup() {
     const { filePath, url, uploadImage } = useStorage()
     const { error, addDoc } = useCollection('playlists')
     const { user } = getUser()
+    const router = useRouter()
+
     const title = ref('')
     const description = ref('')
     const file = ref(null)
     const fileError = ref(null)
     const isPending = ref(false)
-
+    
     const handleSubmit = async () => {
       if (file.value) {
         isPending.value = true
         await uploadImage(file.value)
-        await addDoc({
+        const res = await addDoc({
           title: title.value,
           description: description.value,
           userId: user.value.uid,
@@ -48,7 +52,7 @@ export default {
         })
         isPending.value = false
         if (!error.value) {
-          console.log('playlist added')
+          router.push({ name: 'PlaylistDetails', params: {id: res.id }})
         }
       }
     }
